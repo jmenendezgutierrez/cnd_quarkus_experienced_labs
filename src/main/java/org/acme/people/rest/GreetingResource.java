41f9fa6cbfb5336a0960616bee5bd82e402e676e
jmenendezgutierrez-accenture <j.menendez.gutierrez@accenture.com>
2021-08-06 16:04:20 +0000

    Build jar

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -8,14 +8,26 @@ import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import java.util.Optional;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue="!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
 
     public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return message + " " + name.orElse("world") + suffix;
     }
 }
